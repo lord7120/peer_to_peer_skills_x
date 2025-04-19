5fba3f1835726c0ee688c2439b08538d56b6ea7d
lord7120 <lordindian269@gmail.com>
2025-04-20 00:39:46 +0530

    env created and run on local , error in backend

--- server/db.ts
+++ server/db.ts
@@ -1,4 +1,6 @@
+import 'dotenv/config';
 import { Pool, neonConfig } from '@neondatabase/serverless';
+
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
