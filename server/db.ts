e0a224af3c4c8d433da9cacbdb2e1480f6c0d5df
lord7120 <lordindian269@gmail.com>
2025-06-10 20:47:23 +0530

    test1

--- server/db.ts
+++ server/db.ts
@@ -8,12 +8,18 @@ dotenv.config();
 
 const { Pool } = pkg;
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: Number(process.env.DB_PORT),
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
